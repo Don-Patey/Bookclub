79fa3827df6211dd7a05865fe4f3fcad9bddcfb1
AzukiCoconut <136940320+AzukiCoconut@users.noreply.github.com>
2023-11-03 21:59:26 -0300

    Added login page, seeds for the db, Join Request model, session cookies and join front end to backend.

--- controller/api/userroutes.js
+++ controller/api/userroutes.js
@@ -1,13 +1,16 @@
-const express = require('express');
-const router = express.Router();
-const Users = require('../../models/Users');
-const bcrypt = require('bcrypt');
+const router = require('express').Router();
+const { Users } = require('../../models');
 
 // create a new user    
-router.post('/register', async (req, res) => {
+router.post('/', async (req, res) => {
     try {
-        const userData = await Users.create(req, body);
-        res.status(200).json(userData);
+        const userData = await Users.create(req.body);
+
+        req.session.save(() => {
+          req.session.user_id = userData.id,
+          req.session.loggedIn = true;
+          res.status(200).json(userData);
+        });
     } catch (err) {
         res.status(400).json(err);
     }
@@ -17,44 +20,28 @@ router.post('/register', async (req, res) => {
 router.post('/login', async (req, res) => {
     try {
         const userData = await Users.findOne({ where: { email: req.body.email } });
+
         if (!userData) {
             res.status(400).json({ message: 'Incorrect email or password, please try again' });
             return;
         }
-        const validPassword = await bcrypt.compare(req.body.password, userData.password);
+        const validPassword = await userData.checkPassword(req.body.password);
+
         if (!validPassword) {
             res.status(400).json({ message: 'Incorrect email or password, please try again' });
             return;
         }
-        res.status(200).json({ message: 'You are now logged in!' });
+
+        req.session.save(() => {
+          req.session.user_id = userData.id;
+          req.session.loggedIn = true;
+
+          res.json({user: userData, message: 'You are now logged in!'});
+        });
     } catch (err) {
         res.status(400).json(err);
     }
 });
-
-// Get all users
-router.get('/users', async (req, res) => {
-    try {
-      const users = await Users.findAll();
-      res.status(200).json(users);
-    } catch (err) {
-      res.status(500).json(err);
-    }
-  });
-  
-  // Get a specific user by ID
-  router.get('/users/:id', async (req, res) => {
-    try {
-      const user = await Users.findByPk(req.params.id);
-      if (!user) {
-        res.status(404).json({ message: 'User not found' });
-      } else {
-        res.status(200).json(user);
-      }
-    } catch (err) {
-      res.status(500).json(err);
-    }
-  });
   
   // Update a user by ID
   router.put('/users/:id', async (req, res) => {
@@ -83,6 +70,16 @@ router.get('/users', async (req, res) => {
       }
     } catch (err) {
       res.status(500).json(err);
+    }
+  });
+  
+  router.post('/logout', (req, res) => {
+    if (req.session.loggedIn) {
+      req.session.destroy(() => {
+        res.status(204).end();
+      });
+    } else {
+      res.status(404).end();
     }
   });
   
